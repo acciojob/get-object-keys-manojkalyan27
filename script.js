8b2bfb580e636f03628ed8207823a360ff148d62
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-21 19:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John Doe",
+};
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Example usage
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
